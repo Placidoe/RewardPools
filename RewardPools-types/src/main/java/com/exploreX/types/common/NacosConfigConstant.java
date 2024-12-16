2d8ed6cf81b2bc8b806bcfbfb44fc8f4d263d79c
lixiang1 <lixiang1@huya.com>
2024-12-16 16:31:00 +0800

    feat(): 补充nacos配置中心模板字符串

---
+++ RewardPools-types/src/main/java/com/exploreX/types/common/NacosConfigConstant.java
@@ -0,0 +1,13 @@
+package com.exploreX.types.common;
+
+/**
+ * TODO
+ *
+ * @Description
+ * @Author Lx
+ * @Date 2024/12/16 下午1:56
+ **/
+public class NacosConfigConstant {
+    public static final String NACOS_COMMON_TEMPLATE = "nacos.server.addr";
+    public static final String NACOS_ACTIVE_TEMPLATE = "nacos.server.addr";
+}
